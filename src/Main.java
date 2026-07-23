aaa8132f0134cf25b15d503d323c0cda5728105f
Cauã Graeff Fernandes <cauagraefffernandes@hotmail.com>
2026-07-23 17:15:17 -0300

    versão 1: estruturando o código: removendo simbolos e acentos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.text.Normalizer;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Bem vindo ao Detector de Golpes Digitais!");
+    public static String limparTexto(String texto) {
+        if (texto == null) {
+            return null;
+        }
+
+        // 1. Decompõe os caracteres acentuados em caractere base + acento
+        String normalizado = Normalizer.normalize(texto, Normalizer.Form.NFD);
+
+        // 2. Expressão regular:
+        // \\p{InCombiningDiacriticalMarks} remove os acentos (diacríticos)
+        // [^\p{Alnum}\s] remove tudo que não é letra
+        Pattern padrao = Pattern.compile("\\p{InCombiningDiacriticalMarks}|[^\\p{Alnum}\\s]");
+
+        return padrao.matcher(normalizado).replaceAll("");
     }
-}
+
+        public static void main(String[] args) {
+            System.out.println("Bem vindo ao Detector de Golpes Digitais!");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Digite a mensagem para verificar: ");
+            String mensagem = sc.nextLine();
+            System.out.println(limparTexto(mensagem));
+        }
+    }
+
+
+
+
+
