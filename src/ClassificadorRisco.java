3dcfed19188491b8cd48f1ee0eed6d693432f1de
Jean Luis Machado <jeanluis.dev@gmail.com>
2026-07-30 15:24:39 -0300

    Modificação descartada

--- src/ClassificadorRisco.java
+++ src/ClassificadorRisco.java
@@ -5,13 +5,13 @@ public class ClassificadorRisco {
 
     public String classificar(int pontuacao) {
         if (pontuacao >= LIMITE_RISCO_ALTO) {
-            return "ALTO";
+            return "POSSIVELMENTE GOLPE(TENHA MUITO CUIDADO!)";
         }
 
         if (pontuacao >= LIMITE_RISCO_MEDIO) {
-            return "MÉDIO";
+            return "SUSPEITO(VERIFIQUE A FONTE)";
         }
 
-        return "BAIXO";
+        return "POSSIVELMENTE LEGÍTIMO";
     }
 }
